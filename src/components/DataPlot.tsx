ecd15649414c66e2862cb3a3412d6bf7337825fd
Thiago Saife Rodrigues <ts@storyblok.com>
2022-09-05 17:03:51 -0300

    fixing types

--- src/components/DataPlot.tsx
+++ src/components/DataPlot.tsx
@@ -28,11 +28,22 @@ ChartJS.register(
 type TProps = {
   dataPlot: any[]
 }
+interface IDevice {
+  deviceid: string,
+  fromts: number,
+  tots: number,
+  metrics?: {
+    [key: string]: {
+      [key: string]: number
+    }
+  }
+  value?: number,
+}
 
 export default function DataPlot(dataPlot: TProps) {
-  const [idle, setIdle] = useState([] as number[])
-  const [loaded, setLoaded] = useState([] as number[])
-  const [unloaded, setUnloaded] = useState([] as number[])
+  const [idle, setIdle] = useState([] as IDevice[])
+  const [loaded, setLoaded] = useState([] as IDevice[])
+  const [unloaded, setUnloaded] = useState([] as IDevice[])
   const [labels, setLabels] = useState([] as string[])
 
   const desiredLoadFactor = 0.4 // considering the average from the status loaded
@@ -42,24 +53,24 @@ export default function DataPlot(dataPlot: TProps) {
     handleData(dataPlot.dataPlot)
   }, [dataPlot]);
 
-  function handleData(data: any[]) {
-    const fList = data.map((item: any) => {
+  function handleData(data: IDevice[]) {
+    const fList = data.map((item: IDevice) => {
       if (item.metrics) {
         item.value = item.metrics.Psum.avgvalue * desiredLoadFactor
       }
       return item
     })
 
-    const idle = fList.filter((item: any) => item.value < 20)
+    const idle = fList.filter((item: IDevice) => item.value ? item.value < 20 : false)
     setIdle(idle)
     
-    const loaded = fList.filter((item: any) => item.value >= 20 && item.value <= 100)
+    const loaded = fList.filter((item: IDevice) => item.value ? item.value >= 20 && item.value <= 100 : false)
     setLoaded(loaded)
 
-    const unloaded = fList.filter((item: any) => item.value <= 0.1)
+    const unloaded = fList.filter((item: IDevice) => item.value ? item.value <= 0.1 : false)
     setUnloaded(unloaded)
 
-    const labels = fList.map((item: any) => {
+    const labels = fList.map((item: IDevice) => {
       return new Intl.DateTimeFormat('en',{
         year: 'numeric', month: 'numeric', day: 'numeric',
         hour: 'numeric', minute: 'numeric', second: 'numeric'
@@ -91,21 +102,21 @@ export default function DataPlot(dataPlot: TProps) {
       {
         fill: true,
         label: 'Loaded - 20% >= n <= 100%',
-        data: loaded.map((item: any) => item.value.toFixed(2)),
+        data: loaded.map((item: IDevice) => item.value ? item.value.toFixed(2) : 0),
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
       {
         fill: true,
         label: 'Idle - n < 20%',
-        data: idle.map((item: any) => item.value.toFixed(2)),
+        data: idle.map((item: IDevice) => item.value ? item.value.toFixed(2) : 0),
         borderColor: 'rgb(178, 222, 39)',
         backgroundColor: 'rgba(178, 222, 39, 0.5)',
       },
       {
         fill: true,
         label: 'Unloaded - no current ≈ 0.1A',
-        data: unloaded.map((item: any) => item.value.toFixed(2)),
+        data: unloaded.map((item: IDevice) => item.value ? item.value.toFixed(2) : 0),
         borderColor: 'rgb(53, 162, 235)',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
